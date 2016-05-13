eaea8711ccda1a5567a18371861366dffa057617
DanielKowalczyk1984 <daniel.kowalczyk1984@gmail.com>
2016-05-13 23:00:41 +0200

    Added open_command

--- zsh/20_aliases.zsh
+++ zsh/20_aliases.zsh
@@ -1,6 +1,31 @@
 # Aliases in this file are bash and zsh compatible
 
-# Open files with the default app. Function is defined in oh-my-zsh.
+function open_command() {
+  emulate -L zsh
+  setopt shwordsplit
+
+  local open_cmd
+
+  # define the open command
+  case "$OSTYPE" in
+    darwin*)  open_cmd='open' ;;
+    cygwin*)  open_cmd='cygstart' ;;
+    linux*)   open_cmd='xdg-open' ;;
+    msys*)    open_cmd='start ""' ;;
+    *)        echo "Platform $OSTYPE not supported"
+              return 1
+              ;;
+  esac
+
+  # don't use nohup on OSX
+  if [[ "$OSTYPE" == darwin* ]]; then
+    $open_cmd "$@" &>/dev/null
+  else
+    nohup $open_cmd "$@" &>/dev/null
+  fi
+}
+
+# Open files with the default app.
 alias o="open_command"
 # Get operating system
 platform='unknown'
